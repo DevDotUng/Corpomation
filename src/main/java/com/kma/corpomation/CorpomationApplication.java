f1221dffb5646d8475c2079f9e68aa5358eda739
ung <wkdu4510@gmail.com>
2025-01-10 10:06:43 +0900

    [신규] 프로젝트 생성

---
+++ src/main/java/com/kma/corpomation/CorpomationApplication.java
@@ -0,0 +1,13 @@
+package com.kma.corpomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpomationApplication.class, args);
+	}
+
+}
