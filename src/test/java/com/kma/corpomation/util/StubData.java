fd78e234b06b6a9d96b02480ba6f499619223e24
ung <wkdu4510@gmail.com>
2025-01-13 21:23:10 +0900

    test: MockMultipartFile의 fileName을 파라미터로 받을 수 있도록 변경

--- src/test/java/com/kma/corpomation/util/StubData.java
+++ src/test/java/com/kma/corpomation/util/StubData.java
@@ -6,8 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class StubData {
-    public static MockMultipartFile createMockMultipartFile() throws IOException {
-        final String fileName = "testFile";
+    public static MockMultipartFile createMockMultipartFile(String fileName) throws IOException {
         final String contentType = "pdf";
         final String filePath = "src/test/resources/testFile/"+fileName+"."+contentType;
         FileInputStream fileInputStream = new FileInputStream(filePath);
