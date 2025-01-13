5b0532f9fc68c4208eabe0055be1260dfee4d5e1
ung <wkdu4510@gmail.com>
2025-01-13 10:08:28 +0900

    feat: 파일 조회, 업로드, 삭제를 위한 Controller 구현

--- src/main/java/com/kma/corpomation/domain/admin/controller/AdminController.java
+++ src/main/java/com/kma/corpomation/domain/admin/controller/AdminController.java
@@ -1,13 +1,49 @@
 package com.kma.corpomation.domain.admin.controller;
 
+import com.kma.corpomation.domain.admin.dto.FileResponse;
+import com.kma.corpomation.domain.admin.dto.UploadRequest;
+import com.kma.corpomation.domain.admin.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    private FileService fileService;
+
     @GetMapping("/index")
-    public String admin(){
+    public String admin(Model model) {
+        List<FileResponse> fileResponseList = fileService.getFiles();
+
+        model.addAttribute("files", fileResponseList);
+        System.out.println("length = " + fileResponseList.size());
+
         return "index";
+    }
+
+    @PostMapping("/upload")
+    public String upload(
+            @RequestParam String manager,
+            @RequestParam String business,
+            @RequestParam("file") MultipartFile file) {
+        UploadRequest uploadRequest = new UploadRequest(manager, business, file);
+
+        fileService.uploadFile(uploadRequest);
+        List<FileResponse> fileResponseList = fileService.getFiles();
+
+        return "redirect:/index";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        fileService.deleteFile(id);
+
+        return "redirect:/index";
     }
 }
