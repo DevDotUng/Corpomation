b8f42aac464bcb902969723582a1db26e9164c27
ung <wkdu4510@gmail.com>
2025-01-13 20:41:35 +0900

    feat: 담당자명 및 사업명으로 파일 존재 여부 확인 쿼리 구현

--- src/main/java/com/kma/corpomation/domain/admin/repository/FileRepository.java
+++ src/main/java/com/kma/corpomation/domain/admin/repository/FileRepository.java
@@ -3,4 +3,6 @@ package com.kma.corpomation.domain.admin.repository;
 import com.kma.corpomation.domain.admin.entity.BusinessFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<BusinessFile, Long> {}
+public interface FileRepository extends JpaRepository<BusinessFile, Long> {
+    boolean existsByManagerAndBusiness(String manager, String business);
+}
