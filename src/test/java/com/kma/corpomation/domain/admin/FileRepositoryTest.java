12f31733a592eee761563975a5bb73e86ccb2512
ung <wkdu4510@gmail.com>
2025-01-13 20:42:26 +0900

    test: existsByManagerAndBusiness 쿼리 Test 작성

--- src/test/java/com/kma/corpomation/domain/admin/FileRepositoryTest.java
+++ src/test/java/com/kma/corpomation/domain/admin/FileRepositoryTest.java
@@ -6,6 +6,8 @@ import com.kma.corpomation.domain.admin.repository.FileRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FileRepositoryTest extends ApiTest {
 
     @Autowired
@@ -15,5 +17,18 @@ public class FileRepositoryTest extends ApiTest {
     void delete() {
         fileRepository.save(new BusinessFile("manager", "business", "fileUrl"));
         fileRepository.deleteById(1L);
+    }
+
+    @Test
+    void existsByManagerAndBusiness() {
+        fileRepository.save(new BusinessFile("manager", "business", "fileUrl"));
+
+        boolean exists = fileRepository.existsByManagerAndBusiness("manager", "business");
+        boolean notExistsManager = fileRepository.existsByManagerAndBusiness("business", "business");
+        boolean notExistsBusiness = fileRepository.existsByManagerAndBusiness("manager", "manager");
+
+        assertThat(exists).isTrue();
+        assertThat(notExistsManager).isFalse();
+        assertThat(notExistsBusiness).isFalse();
     }
 }
