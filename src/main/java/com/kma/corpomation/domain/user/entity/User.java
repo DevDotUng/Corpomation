d6613a2080a344a01ccfdac1c7dd1baa2cd140c7
ung <wkdu4510@gmail.com>
2025-01-18 15:40:31 +0900

    feat: 로그인을 위한 User Entity 정의

---
+++ src/main/java/com/kma/corpomation/domain/user/entity/User.java
@@ -0,0 +1,47 @@
+package com.kma.corpomation.domain.user.entity;
+
+import com.kma.corpomation.domain.user.dto.UserRole;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    public User(final String username, final String email, final String password, final UserRole userRole) {
+        Assert.hasText(username, "이름은 필수입니다.");
+        Assert.hasText(email, "이메일은 필수입니다.");
+        Assert.hasText(password, "비밀번호는 필수입니다.");
+        Assert.hasText(userRole.value(), "유저 권한은 필수입니다.");
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
+}
