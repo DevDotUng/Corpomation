9507a821bc3561cc244bee9df3e7cc24125625bd
ung <wkdu4510@gmail.com>
2025-01-19 17:01:17 +0900

    feat: User Entity에 Setter 추가

--- src/main/java/com/kma/corpomation/domain/user/entity/User.java
+++ src/main/java/com/kma/corpomation/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.util.Assert;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
