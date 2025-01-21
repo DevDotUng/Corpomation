63ffb6cf3029a99e52e80c76cbb30640ef77b158
ung <wkdu4510@gmail.com>
2025-01-22 00:17:50 +0900

    feat: Jwt 기능 구현

---
+++ src/main/java/com/kma/corpomation/domain/user/jwt/JwtUtil.java
@@ -0,0 +1,20 @@
+package com.kma.corpomation.domain.user.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+
+@Service
+public class JwtUtil {
+    public Key getSigningKey(String secretKey) {
+        String encodedKey = encodeToBase64(secretKey);
+        return Keys.hmacShaKeyFor(encodedKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String encodeToBase64(String secretKey) {
+        return Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+}
